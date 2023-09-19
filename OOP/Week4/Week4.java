7752189dbf0565377c68d7c0765de094326f2f31
22021202-VU-VAN-HUY <22021202@vnu.edu.vn>
2023-09-19 11:53:58 +0700

    oop

---
+++ OOP/Week4/Week4.java
@@ -0,0 +1,14 @@
+public class Week4 {
+    public static int max2Int(int a, int b) {
+	// Tim gia tri lon nhat cua hai so nguyen
+    }
+
+    public static int minArray (int[] array) {
+	// Tim gia tri nho nhat cua 1 mang so nguyen
+    }
+
+    public static String calculateBMI(double weight, double height){
+	// Tinh BMI
+    }
+}
+ 
---
+++ OOP/Week4/Week4Test.java
@@ -0,0 +1,48 @@
+import org.junit.Test;
+
+public class Week4Test {
+    @Test
+    public void testMax2Int1(){}
+
+    @Test
+    public void testMax2Int2(){}
+
+    @Test
+    public void testMax2Int3(){}
+
+    @Test
+    public void testMax2Int4(){}
+
+    @Test
+    public void testMax2Int5(){}
+
+    @Test
+    public void testMinArray1(){}
+
+    @Test
+    public void testMinArray2(){}
+
+    @Test
+    public void testMinArray3(){}
+
+    @Test
+    public void testMinArray4(){}
+
+    @Test
+    public void testMinArray5(){}
+
+    @Test
+    public void testCalculateBMI1(){}
+
+    @Test
+    public void testCalculateBMI2(){}
+
+    @Test
+    public void testCalculateBMI3(){}
+
+    @Test
+    public void testCalculateBMI4(){}
+
+    @Test
+    public void testCalculateBMI5(){}
+} 
